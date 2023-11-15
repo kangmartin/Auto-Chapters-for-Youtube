44cc8a188d2c77f7415ed901531a701a8873d82f
Martin Kang <martin.kang@efrei.net>
2023-11-15 22:47:11 +0100

    Add console logs and error handling for subtitle button and extraction

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-// Fonction pour cliquer sur le bouton des sous-titres
 function activerSousTitres() {
     return new Promise((resolve, reject) => {
         const boutonSousTitres = document.querySelector("#primary-button > ytd-button-renderer > yt-button-shape > button");
 
         if (boutonSousTitres) {
+            console.log("Bouton des sous-titres cliqué");
             boutonSousTitres.click();
             resolve();
         } else {
+            console.error('Bouton des sous-titres non trouvé.');
             reject('Bouton des sous-titres non trouvé.');
         }
     });
 }
 
-// Fonction pour extraire les sous-titres
 function extraireSousTitres() {
     return new Promise((resolve, reject) => {
         const segments = document.querySelectorAll('ytd-transcript-segment-renderer');
 
         if (segments.length === 0) {
+            console.error('Aucun sous-titre trouvé.');
             reject('Aucun sous-titre trouvé.');
             return;
         }
@@ -28,24 +29,30 @@ function extraireSousTitres() {
             return { timestamp, texte };
         });
 
+        console.log("Sous-titres extraits: ", sousTitres);
         resolve(sousTitres);
     });
 }
 
-// Fonction pour initialiser l'extraction des sous-titres
 function initialiserExtraction() {
     activerSousTitres().then(() => {
         setTimeout(() => {
             extraireSousTitres().then(sousTitres => {
-                // Envoyer les sous-titres à la popup
-                chrome.runtime.sendMessage({
-                    action: "afficherSousTitres",
-                    sousTitres: sousTitres
-                });
-            }).catch(error => console.error(error));
-        }, 3000); // Attendre 3 secondes pour le chargement des sous-titres
-    }).catch(error => console.error(error));
+                window.sousTitresExtraits = sousTitres;
+            }).catch(error => console.error("Erreur lors de l'extraction des sous-titres: ", error));
+        }, 3000);
+    }).catch(error => console.error("Erreur lors de l'activation des sous-titres: ", error));
 }
 
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "demandeSousTitres") {
+    console.log("Demande de sous-titres reçue");
+    chrome.runtime.sendMessage({
+      action: "afficherSousTitres",
+      sousTitres: window.sousTitresExtraits
+    });
+  }
+});
 
 setTimeout(initialiserExtraction, 2000);
