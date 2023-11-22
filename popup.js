f37d4b19c3b165a85bdccdf8b4dde461bec0537b
Martin Kang <martin.kang@efrei.net>
2023-11-22 23:08:05 +0100

    Update popup.js and style.css

--- popup.js
+++ popup.js
@@ -2,12 +2,19 @@ document.getElementById('getSubtitles').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "demandeSousTitres"});
     });
-  });
-  
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "afficherSousTitres") {
-      const sousTitres = request.sousTitres.map(st => `<p><b>${st.timestamp}</b>: ${st.texte}</p>`).join('');
-      document.getElementById("subtitles").innerHTML = sousTitres;
+        // Débuter avec une balise <ul> vide
+        let listeSousTitres = '<ul>';
+        // Ajouter chaque sous-titre dans un élément <li>
+        request.sousTitres.forEach(st => {
+            listeSousTitres += `<li><b>${st.timestamp}</b>: ${st.texte}</li>`;
+        });
+        // Fermer la balise <ul>
+        listeSousTitres += '</ul>';
+        // Mettre à jour le contenu de l'élément pour afficher la liste
+        document.getElementById("subtitles").innerHTML = listeSousTitres;
     }
-  });
-  
+});
