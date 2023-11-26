b2dec7d490b1d8e812424188fd28dcab720a7cd6
Olivier2Carglass <104084572+oliver2Carglass@users.noreply.github.com>
2023-11-26 07:40:12 -0400

    interface totalement mise a jour
    changement de l'interface et des animations visuelles

--- popup.js
+++ popup.js
@@ -1,4 +1,39 @@
-document.getElementById('getSubtitles').addEventListener('click', () => {
+const button =document.getElementById('getSubtitles')
+
+function dsbl(){
+    button.disabled = true;
+
+    button.style.backgroundColor = 'gray';
+    button.style.color = '#aaa';
+    button.style.border = '3px solid #ccc';
+    button.style.cursor = 'not-allowed';
+
+    // Attendre 2 secondes (2000 millisecondes) avant de réactiver le bouton
+    setTimeout(function() {
+        button.disabled = false;
+
+        button.style.backgroundColor = 'transparent';
+        button.style.color = 'rgb(255, 255, 255)';
+        button.style.border = '3px solid rgb(252, 70, 100)';
+        button.style.cursor = 'pointer';
+    }, 5000);
+}
+
+
+// function desactiverBoutonTemporairement() {
+//     var bouton = document.getElementById('getSubtitles');
+//     bouton.disabled = true;
+
+//     // Appliquer le style spécifique au bouton désactivé
+//     // bouton.style.backgroundColor = 'gray';
+//     // bouton.style.color = '#aaa';
+//     // bouton.style.border = '3px solid #ccc';
+//     // bouton.style.cursor = 'not-allowed';
+
+
+dsbl()
+button.addEventListener('click', () => {
+    button.remove()
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "demandeSousTitres"});
     });
@@ -10,7 +45,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         let listeSousTitres = '<ul>';
         // Ajouter chaque sous-titre dans un élément <li>
         request.sousTitres.forEach(st => {
-            listeSousTitres += `<li><b>${st.timestamp}</b>: ${st.texte}</li>`;
+            listeSousTitres += `<li><b>${st.timestamp}</b><p>${st.texte}</p></li>`;
         });
         // Fermer la balise <ul>
         listeSousTitres += '</ul>';
@@ -18,3 +53,4 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         document.getElementById("subtitles").innerHTML = listeSousTitres;
     }
 });
+
